513edc4b64cba6b8be885245cb43e3ea51d7a8a4
george-kaps <115156208+george-kaps@users.noreply.github.com>
2023-06-20 08:26:06 +0000

    Frist commit

---
+++ 0x00-handouts/homework 0.py
@@ -0,0 +1,40 @@
+**HOMEWORK 0**
+
+  
+def main():
+  fun_of_dict()
+  fun_of_list()
+  fun_of_tuple()
+
+def fun_of_list():
+  my_list[]
+  inp_lst = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+  list.append('fig')
+  size = len(inp_lst)
+  print(size)
+  print list[3]
+
+  l . remove (5)
+
+print(my_list)
+  pass
+
+def fun_of_tuple():
+  # Empty tuple
+  my_tuple = (10, 20, 30, 40, 50)
+  print "tuple length : "
+  print ("element at the index 4 is: " , tup[4])
+  
+  pass
+
+def fun_of_dict():
+  my_dictionary = my_dict{'name': john, 'age': 25, 'counry': USA}
+  print(key, name)
+  dict.update(age = 26)
+  iskeypresent = county in dict
+print(my_dict)
+  pass
+
+if __name__ == "__main__":
+  main()```
+
